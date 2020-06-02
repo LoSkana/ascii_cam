ac2e1e6ecbc56dca7fbb2ba36de3a26f2c5b8f05
loskana <5333290+LoSkana@users.noreply.github.com>
2020-06-02 12:58:18 +0200

    up

--- site/client/views.py
+++ site/client/views.py
@@ -8,22 +8,34 @@ import time
 import datetime
 from datetime import timezone
 
-def index(request):
-    return render(request, 'index.html')
+import random
+import string
+
+def randomString(stringLength=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+def index(request):    
+    while True:
+        alias = randomString()
+        if cache.get('c-' + alias) is None:
+            break
+    return render(request, 'index.html', {'alias': alias })
        
 
 @csrf_exempt
 def send(request):
     # print(request.POST)
     d = request.POST.get("d", "")
-    a = request.POST.get("a", "")
+    alias = request.POST.get("a", "")
+    room = request.POST.get("r", "")
     
     # save info about this frame 
-    cache.set('c-' + a, d) 
+    cache.set('c-' + alias, d) 
     dt = datetime.datetime.now()
     timestamp = int(dt.replace(tzinfo=timezone.utc).timestamp())
-    cache.set('l-' + a, timestamp)
-    o_dic = cache.get('d')
+    cache.set('l-' + alias, timestamp)
+    o_dic = cache.get('r-' + room)
     if o_dic is None:
         o_dic = {}
         
@@ -33,7 +45,8 @@ def send(request):
     # get other webcams frame
     for o_a in o_dic.keys(): 
         if a == o_a:
-            continue
+            pass
+            #continue
         last_seen = int(cache.get('l-' + o_a))
         if timestamp - last_seen > 3:
             dic.pop(o_a, None)
@@ -43,6 +56,6 @@ def send(request):
         res[o_a] = cache.get('c-' + o_a)
     
     dic[a] = 1
-    cache.set('d', dic)
+    cache.set('r-' + room, dic)
 
     return JsonResponse(res)
